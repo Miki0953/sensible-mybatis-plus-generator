9d7cc36815f67db02dcba8f8b2d74d66def5e71e
liuyc <425449643@qq.com>
2020-03-29 22:46:27 +0800

    初始化项目

---
+++ src/main/java/com/sensible/MybatisPlusApplication.java
@@ -0,0 +1,14 @@
+package com.sensible;
+
+import com.sensible.model.LeaguerInfo;
+
+/**
+ * @author liuyc
+ * @Description:
+ * @date 2020/3/29 002920:30
+ */
+public class MybatisPlusApplication {
+    public static void main(String[] args) {
+        LeaguerInfo leaguerInfo = new LeaguerInfo();
+    }
+}
---
+++ src/test/java/com/sensible/SensibleMytatisPlusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sensible;
+
+import com.sensible.mapper.LeaguerInfoMapper;
+import com.sensible.model.LeaguerInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensibleMytatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
